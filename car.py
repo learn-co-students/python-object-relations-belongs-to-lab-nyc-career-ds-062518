6ef0a40516d6a07194ab0ea01e902e173ee787e0
Paulina Zheng <paulinaczheng@gmail.com>
2018-07-03 11:37:34 -0400

    Done.

--- car.py
+++ car.py
@@ -8,3 +8,56 @@ class Car:
         self._year = year
         self._owner = owner
         Car._all.append(self)
+
+    @property
+    def make(self):
+        return self._make
+
+    @make.setter
+    def make(self, make):
+        self._make = make
+
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def model(self, model):
+        self._model = model
+
+    @property
+    def year(self):
+        return self._year
+
+    @year.setter
+    def year(self, year):
+        self._year = year
+
+    @property
+    def owner(self):
+        return self._owner
+
+    @owner.setter
+    def owner(self, owner):
+        self._owner = owner
+
+    @classmethod
+    def all(cls):
+        return cls._all
+
+    @classmethod
+    def oldest_car(cls, OtherClass):
+        car_year_min = min([car.year for car in cls._all])
+        return [car.owner for car in cls._all if car.year == car_year_min][0]
+
+    @classmethod
+    def make_owner(cls, OtherClass, make):
+        return [car.owner for car in cls._all if car.make == make]
+
+    @classmethod
+    def return_make(cls, owner):
+        return [car.make for car in cls._all if car.owner == owner][0]
+
+    @classmethod
+    def cars_driven_by(cls, occupation):
+        return [car for car in cls._all if car.owner.occupation == occupation]
