f895780f3c6f04cc40753ffd1fc6748159960273
Sam Liebman <sammyliebman@gmail.com>
2018-07-03 11:38:47 -0400

    Done.

--- car.py
+++ car.py
@@ -8,3 +8,53 @@ class Car:
         self._year = year
         self._owner = owner
         Car._all.append(self)
+
+    @classmethod
+    def all(cls):
+        return cls._all
+
+    @property
+    def make(self):
+        return self._make
+    @make.setter
+    def make(self, make):
+        self._make = make
+
+    @property
+    def model(self):
+        return self._model
+    @model.setter
+    def model(self, model):
+        self._model = model
+
+    @property
+    def year(self):
+        return self._year
+    @year.setter
+    def year(self, year):
+        self._year = year
+
+    @property
+    def owner(self):
+        return self._owner
+    @owner.setter
+    def owner(self, owner):
+        self._owner = owner
+
+    @classmethod
+    def has_oldest_car(cls, OtherClass):
+        oldest_car = min([car._year for car in cls._all])
+        return [car._owner for car in cls._all if car._year == oldest_car][0]
+
+    @classmethod
+    def drives_a(cls, OtherClass, make):
+        return [car._owner for car in cls._all if car._make == make]
+
+    @classmethod
+    def return_make(cls, owner):
+        return [car._make for car in cls._all if car._owner == owner][0]
+
+    @classmethod
+    def cars_driven_by(cls, occupation):
+        list = [car for car in cls._all if car.owner.occupation == occupation]
+        return list
--- person.py
+++ person.py
@@ -3,6 +3,46 @@ from car import Car
 
 class Person:
 
+    # _all = []
+
     def __init__(self, name, occupation):
         self._name = name
         self._occupation = occupation
+        # Person._all.append(self)
+
+    # @classmethod
+    # def all(cls):
+    #     return cls._all
+
+    @property
+    def name(self):
+        return self._name
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def occupation(self):
+        return self._occupation
+    @occupation.setter
+    def occupation(self, occupation):
+        self._occupation = occupation
+
+    @classmethod
+    def has_oldest_car(cls):
+        return Car.has_oldest_car(cls)
+
+    @classmethod
+    def drives_a(cls, make):
+        return Car.drives_a(cls, make)
+
+    def drives_same_make_as_me(self):
+        #find make associated with instance
+        make = Car.return_make(self)
+        owner_list = Person.drives_a(make)
+        owner_list.remove(self)
+        return owner_list
+
+    # @classmethod
+    # def cars_driven_by(cls, OtherClass, occupation):
+    #     return [person for person in cls._all if person._occupation == occupation]
