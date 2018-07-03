7184d2354e516f66e6f858b772ff05dcdc85b0e9
David Masse <davidjmasse@hotmail.com>
2018-07-03 12:32:33 -0400

    Done.

--- person.py
+++ person.py
@@ -1,4 +1,4 @@
-# import car class here
+
 from car import Car
 
 class Person:
@@ -6,3 +6,32 @@ class Person:
     def __init__(self, name, occupation):
         self._name = name
         self._occupation = occupation
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def occupation(self):
+        return self._occupation
+
+    @occupation.setter
+    def occupation(self, occupation):
+        self._occupation = occupation
+
+    @classmethod
+    def has_oldest_car(cls):
+        oldcar = min(Car.all(), key = lambda car: car._year)
+        return oldcar._owner
+
+    @classmethod
+    def drives_a(cls, make):
+        return [car._owner for car in Car.all() if car._make == make]
+
+    def drives_same_make_as_me(self):
+        mymake = [car._make for car in Car.all() if car._owner == self][0]
+        return [car._owner for car in Car.all() if car._make == mymake and car._owner != self]
